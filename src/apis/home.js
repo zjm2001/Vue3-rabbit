1616342db614dbae3fdd58ebcf26374041679c9c
zjm <2215539769@qq.com>
2023-08-07 16:34:13 +0800

    封装了公用组件,完成新鲜好物.

--- src/apis/home.js
+++ src/apis/home.js
@@ -5,3 +5,10 @@ export function getBannerAPI (){
         url:'/home/banner'
     })
 }
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findNewAPI= ()=>http({url:'/home/new'})
+    
---
+++ src/views/Home/components/HomePanel.vue
@@ -0,0 +1,62 @@
+<script setup>
+defineProps({
+    title: {
+        type: String,
+        default: '',
+        required: true
+    },
+    SubTitle: {
+        type: String,
+        default: '',
+        required: true
+    }
+})
+
+</script>
+
+
+<template>
+    <div class="home-panel">
+        <div class="container">
+            <div class="head">
+                <!-- 主标题和副标题 -->
+                <h3>
+                    {{ title }}<small>{{ SubTitle }}</small>
+                </h3>
+            </div>
+            <!-- 主体内容区域 -->
+            <div>
+                <!-- 放置一个插槽     -->
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang='scss'>
+.home-panel {
+    background-color: #fff;
+   
+
+    .head {
+        padding: 40px 0;
+        display: flex;
+        align-items: flex-end;
+
+        h3 {
+            flex: 1;
+            font-size: 32px;
+            font-weight: normal;
+            margin-left: 6px;
+            height: 35px;
+            line-height: 35px;
+
+            small {
+                font-size: 16px;
+                color: #999;
+                margin-left: 20px;
+            }
+        }
+    }
+}
+</style>
