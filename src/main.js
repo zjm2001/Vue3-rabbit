5942e1f91273eb3a048652265287b526789fffb0
zjm <2215539769@qq.com>
2023-08-08 09:40:06 +0800

    实现图片的懒加载自定义指令

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import '@/styles/common.scss'  //引入初始化样式
+import { lazyPlugin } from '@/directives'
 
 import App from './App.vue'
 import router from './router'
@@ -8,5 +9,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+// 全局指令注册
+app.use(lazyPlugin)
 
 app.mount('#app')
