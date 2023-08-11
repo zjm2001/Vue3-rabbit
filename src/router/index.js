22e676ffb66780f883e13e67b772eb4a8181bf53
zjm <2215539769@qq.com>
2023-08-11 15:12:49 +0800

    本地购物车基础列表渲染

--- src/router/index.js
+++ src/router/index.js
@@ -27,7 +27,12 @@ const router = createRouter({
           path: 'detail/:id',
           name:'detail',
           component:  () => import('@/views/Detail/index.vue'),
-        }
+        },
+        {
+          path: 'cartlist',
+          name:'cartlist',
+          component:  () => import('@/views/CartList/index.vue'),
+        },
 
       ]
     },
